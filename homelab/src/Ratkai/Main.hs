b8b19d512e96cdc67f3dee373833b212ea9a0f9f
Gergo ERDI <gergo@erdi.hu>
2023-10-05 22:54:02 +0200

    ZSCII decoder

--- homelab/src/Ratkai/Main.hs
+++ homelab/src/Ratkai/Main.hs
@@ -1,11 +1,103 @@
 {-# LANGUAGE RecordWildCards, RecursiveDo, BlockArguments #-}
+{-# LANGUAGE BinaryLiterals, NumericUnderscores #-}
 module Ratkai.Main (game) where
 
 import Z80
 import Z80.Utils
 import HL2
+import qualified Data.ByteString as BS
+import Control.Monad
 
-game :: Z80ASM
-game = mdo
-    loopForever $ pure ()
-    pure ()
+game :: IO Z80ASM
+game = do
+    text1' <- BS.readFile "/home/cactus/prog/c64/bosszu-disasm/ratbc/_out/text1.zscii"
+
+    pure $ mdo
+        ldVia A [shiftState] 0
+        ld HL text1
+        ld B 29
+        call printZ
+
+        loopForever $ pure ()
+        printZ <- labelled $ withLabel \tryNext -> mdo
+            -- Is this the word we want?
+            dec B
+            jp Z start -- TODO: jr?
+
+            loopForever do
+                inc HL
+                ld A [HL]
+                inc HL
+                bit 7 A
+                jp NZ tryNext
+
+            start <- labelled $ loopForever do
+                ldVia A E [HL]
+                inc HL
+
+                -- Third character: lowest 5 bits of E
+                Z80.and 0x1f
+                ld [buf + 2] A
+
+                ldVia A D [HL]
+                inc HL
+
+                -- Second character: lowest 2 bits of D with highest 3 bits of E
+                replicateM_ 5 $ srl E
+                replicateM_ 3 $ sla A
+                Z80.or E
+                Z80.and 0x1f
+                ld [buf + 1] A
+
+                -- first character: bits 6..2 of D
+                ld A D
+                replicateM_ 2 $ srl A
+                Z80.and 0x1f
+                ld [buf + 0] A
+
+                -- Finished: bit 7 of D
+                ld A D
+                Z80.and 0b1000_0000
+                ld [isLast] A
+
+                ld IX buf
+                replicateM_ 3 $ call printZ1
+                ld A [isLast]
+                cp 0
+                ret NZ
+            pure ()
+
+
+        printZ1 <- labelled mdo
+            ld A [shiftState]
+            cp 0
+            ldVia A [shiftState] 0
+            jp NZ shifted
+
+            ld A [IX]
+            inc IX
+            sub 6
+            jr C shift
+
+            add A 0x41
+            call 0x1fc
+            ret
+
+            shifted <- labelled do
+                ld A [IX]
+                inc IX
+                -- TODO: it could be something other than space
+                ld A 0x20
+                call 0x1fc
+                ret
+
+            shift <- labelled do
+                ldVia A [shiftState] 1
+            ret
+
+        text1 <- labelled $ db text1'
+        buf <- labelled $ db [0, 0, 0]
+        isLast <- labelled $ db [0]
+        shiftState <- labelled $ db [0]
+
+        pure ()
--- homelab/src/main.hs
+++ homelab/src/main.hs
@@ -6,31 +6,20 @@ import qualified Ratkai.Main as Ratkai
 
 import Z80
 import Z80.Utils
-import Data.Word
 import qualified Data.ByteString as BS
-import Control.Monad
-import Data.Bits
-import Data.Char
-import Text.Printf
 import Data.String (fromString)
 import System.FilePath
 import System.Directory
 
 main :: IO ()
 main = do
-    emit "_build/ratkai" $ org 20000 Ratkai.game
+    emit "_build/ratkai" . org 20000 =<< Ratkai.game
 
 emit :: String -> ASMBlock -> IO ()
 emit name block = do
     createDirectoryIfMissing True (takeDirectory name)
     BS.writeFile (name <.> "obj") $ asmData block
     BS.writeFile (name <.> "htp") $ htp (fromString $ takeBaseName name) block
-
-labelASCII :: Location -> [Word8]
-labelASCII loc = map (+ 0x30) $ digits
-  where
-    -- The length of this has to be lazy in the actual value of `loc`
-    digits = [fromIntegral $ (loc `div` (10 ^ i)) `mod` 10 | i <- [4, 3 .. 0]]
 
 htp :: BS.ByteString -> ASMBlock -> BS.ByteString
 htp label mainBlock = mconcat
